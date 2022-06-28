5265b37978c429eb6267470d2f0eea20a180510d
AngieH <62782529+htbngoc@users.noreply.github.com>
2022-06-28 12:30:44 -0700

    change icons

--- index.js
+++ index.js
@@ -20,6 +20,56 @@ let month = months[now.getMonth()];
 let year = now.getFullYear();
 h1.innerHTML = `${month} ${date}, ${year}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="img/icons/${forecastDay.weather[0].icon}.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperatureCurrent(response) {
   console.log(response);
 
@@ -38,10 +88,8 @@ function showTemperatureCurrent(response) {
     response.data.weather[0].main;
   document
     .querySelector("#icon")
-    .setAttribute(
-      "src",
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
+    .setAttribute("src", `img/icons/${response.data.weather[0].icon}.png`);
+  getForecast(response.data.coord);
 }
 
 function cityShow(city) {
@@ -102,3 +150,4 @@ let temperatureCelsiusShow = document.querySelector("#celsius-link");
 temperatureCelsiusShow.addEventListener("click", temperatureCelsiusFunction);
 
 cityShow("Costa Rica");
+displayForecast();
